c5a56ca37d35739a6dec2085dbddd92220b46970
ClarkeEl360 <ClarkeEl360@northwested.org>
2026-03-09 11:29:10 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// Elliot Clarke
+// 3/9/2026
+// Declartion Assignment
+
+let age = 18;
+const width = 200;
+const birthState = "Michigan";
+
+let firstName = "Elliot";
+let lastName = "Clarke";
+const homeSchool = "TC Central High School";
+const isLoggedIn = true;
+const isAdult = true;
+
