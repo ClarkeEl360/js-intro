c56abc71ed784183794f712f9e74636d0051a8f1
ClarkeEl360 <ClarkeEl360@northwested.org>
2026-03-10 11:16:56 -0400

    Create student-average.js

---
+++ student-average.js
@@ -0,0 +1,16 @@
+// Elliot Clarke
+// 3/10/2026
+// Student Avrage
+
+const quiz1 = 100;
+const quiz2 = 90;
+const quiz3 = 85;
+const quiz4 = 45;
+const quiz5 = 80;
+const quiz6 = 60;
+
+
+const total = quiz1 + quiz2 + quiz3 + quiz4 + quiz5 + quiz6;
+const avg = total/6;
+
+console.log(avg)
