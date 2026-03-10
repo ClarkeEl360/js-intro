be83592922b5702af1f8c372984dbaaa6358b5c7
ClarkeEl360 <ClarkeEl360@northwested.org>
2026-03-10 11:28:51 -0400

    Update student-average.js

--- student-average.js
+++ student-average.js
@@ -2,6 +2,9 @@
 // 3/10/2026
 // Student Avrage
 
+
+const studentFirstName = "Fionna";
+const studentLastName = "Eichler"
 const quiz1 = 100;
 const quiz2 = 90;
 const quiz3 = 85;
@@ -13,4 +16,9 @@ const quiz6 = 60;
 const total = quiz1 + quiz2 + quiz3 + quiz4 + quiz5 + quiz6;
 const avg = total/6;
 
-console.log(avg)
+console.log("Students Full Name: " + studentFirstName + " " + studentLastName)
+console.log("Student Home School and Grade: " + homeSchool + " " + grade + "th grade")
+console.log("Students Avrage Grade: " + avg + "%");
+
+
+
